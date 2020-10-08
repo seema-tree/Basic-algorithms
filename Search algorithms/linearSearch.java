ab576ca9b87b03d6a879e579a2e3e8b826fef755
chirag2506 <chiraggupta2506@gmail.com>
2020-10-08 12:01:05 +0530

    search algo

---
+++ Search algorithms/linearSearch.java
@@ -0,0 +1,23 @@
+//TIME COMPLEXITY = O(n)
+
+class linearSearch  {  
+    public static int search(int arr[], int x) { 
+        int n = arr.length; 
+        for(int i = 0; i < n; i++){ 
+            if(arr[i] == x) 
+                return i; 
+        } 
+        return -1; 
+    } 
+    
+    public static void main(String args[]) { 
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  
+        int x = 7; 
+        
+        int result = search(arr, x); 
+        if(result == -1) 
+            System.out.println("Element is not present in array"); 
+        else
+            System.out.println("Element is present at index " + result); 
+    } 
+} 
